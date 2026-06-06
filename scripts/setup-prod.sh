cfe9505b71a84cae6899442c93219e85e5e7e437
Manoj-Dhundhalva <manoj@titan.email>
2026-06-06 15:27:33 +0530

    [fix]: setup script

--- scripts/setup-prod.sh
+++ scripts/setup-prod.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 echo "Installing dependencies..."
 yarn install
 
-echo "Installing Playwright WebKit..."
-npx playwright install firefox
+echo "Installing Playwright Firefox..."
+PLAYWRIGHT_BROWSERS_PATH=0 npx playwright install firefox
 
 echo "Done."
